88652a69fb5539d63f6679c332bbe934200061b0
Harsha6142611 <pasupulaharsha7006@gmail.com>
2024-11-01 11:03:25 +0530

    updated urls

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -14,24 +14,7 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, tags, link, icon, githubLink, image }) => {
   return (
     <div className="bg-gray-800/50 p-6 rounded-lg hover:bg-gray-700/50 transition-colors relative">
-      {/* {image && <img src={image} alt={title} className="w-full h-32 object-cover rounded-t-lg mb-4" />} */}
-      {/* {image && (
-  <div className="overflow-visible rounded-t-lg mb-4 relative group">
-    <img
-      src={image}
-      alt={title}
-      className="w-full h-32 object-cover transition-transform duration-300"
-    />
-    <div className="absolute top-0 left-0 hidden group-hover:flex items-center justify-center w-full h-full">
-      <img
-        src={image}
-        alt={title}
-        className="w-[400px] h-[300px] object-cover transition-transform duration-300 transform -translate-x-1/2 -translate-y-1/2"
-        style={{ position: 'absolute', top: '50%', left: '50%', zIndex: 10 }}
-      />
-    </div>
-  </div>
-)} */}
+      
 <div className="overflow-visible rounded-t-lg mb-4 relative group">
   <img
     src={image}
@@ -51,11 +34,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, tags, lin
 
       <div className="flex items-center justify-between mb-4">
         {/* <div className="text-blue-400">{icon}</div> */}
-        <a href={link} className="text-gray-400 hover:text-blue-400 transition-colors">
+        <a href={link} className="text-gray-400 hover:text-blue-400 transition-colors" target="_blank">
           <ExternalLink className="w-5 h-5" />
         </a>
         {githubLink && (
-        <a href={githubLink} className="hover:text-blue-400 transition-colors flex items-center gap-2 hover:scale-110 transform duration-200">
+        <a href={githubLink} className="hover:text-blue-400 transition-colors flex items-center gap-2 hover:scale-110 transform duration-200" target="_blank">
           <Github className="w-5 h-5" />
           </a>
         )}
